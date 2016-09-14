5619db70cfbe32e3dc59841f2856d9eb25786d64
hirono <hotarak@gmail.com>
2016-09-15 00:14:14 +0900

    Add name,avatar,member,profile,works columns to users_table

--- db/migrate/20160909053738_devise_create_users.rb
+++ db/migrate/20160909053738_devise_create_users.rb
@@ -2,8 +2,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :avatar
+      t.string :member
+      t.text   :profile
+      t.text   :works
 
       ## Recoverable
       t.string   :reset_password_token
