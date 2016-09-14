a9612949852ac1b7ad134e0a2b7de7933f77d709
hirono <hotarak@gmail.com>
2016-09-14 23:43:42 +0900

    Add index_action to users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes
-  resources :user, only: [:show, :edit, :update]
+  resources :user, only: [:index, :show, :edit, :update]
 end
