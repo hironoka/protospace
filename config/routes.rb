b30acd8ed7f1683d6a747bb799498a4e66baae27
hirono <hotarak@gmail.com>
2016-09-16 17:53:13 +0900

    Correct spell Miss user→users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes
-  resources :user, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
