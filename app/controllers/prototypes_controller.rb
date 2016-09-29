f63a2d95ea4e789ebb9717b7d45d8750d892780b
hirono <hotarak@gmail.com>
2016-09-29 21:06:24 +0900

    get user_id

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :move_to_index, except: [:index]
   before_action :set_prototype, only: [:show, :edit]
-  before_action :set_user, only: [:show]
+  # before_action :set_user, only: [:new, :show]
 
   def index
     @prototypes = Prototype.all
