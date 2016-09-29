4ee35f907e1218c3f66ec89e8b50cc4dc6dfca29
hirono <hotarak@gmail.com>
2016-09-29 13:50:14 +0900

    add title_column to prototypes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909053738) do
+ActiveRecord::Schema.define(version: 20160929044737) do
 
   create_table "prototypes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160909053738) do
     t.text     "concept",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title",      limit: 255
   end
 
   create_table "users", force: :cascade do |t|
