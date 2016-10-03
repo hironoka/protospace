c05877d7881206d53a9c1d3d43715255b248758d
hirono <hotarak@gmail.com>
2016-10-03 23:11:45 +0900

    add dependent option

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :reject_image
 
   validates :title,
