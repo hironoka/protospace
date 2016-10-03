7950feec5f40c78b9cb044ae20981d5951bce314
hirono <hotarak@gmail.com>
2016-10-03 19:18:18 +0900

    add reject_if

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images
-  accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: all_blank
 
   validates :title, :catch_copy, :concept, presence: true
 end
