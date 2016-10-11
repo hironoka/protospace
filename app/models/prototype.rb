199aed0a531031852cd84e8ff70a76adef1a6518
hirono <hotarak@gmail.com>
2016-10-12 00:46:00 +0900

    add limit against sub_images

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images, dependent: :destroy
   has_one :main_image, -> { where(status: 0) }, class_name: "Image"
-  has_many :sub_image, -> { where(status: 1) }, class_name: "Image"
+  has_many :sub_image, -> { where(status: 1).limit(3) }, class_name: "Image"
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :reject_image
 
   validates :title,
