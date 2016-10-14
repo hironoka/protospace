dd80fd4bea960112244345903cee1f971f388e73
hirono <hotarak@gmail.com>
2016-10-15 04:45:21 +0900

    add like model and likes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002163319) do
+ActiveRecord::Schema.define(version: 20161014174044) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 20161002163319) do
     t.datetime "updated_at"
   end
 
-  create_table "prototypes", force: :cascade do |t|
-    t.text     "catch_copy", limit: 65535
-    t.text     "concept",    limit: 65535
+  create_table "likes", force: :cascade do |t|
+    t.integer  "prototype_id", limit: 4
+    t.integer  "user_id",      limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",      limit: 255
-    t.integer  "user_id",    limit: 4
+  end
+
+  create_table "prototypes", force: :cascade do |t|
+    t.text     "catch_copy",  limit: 65535
+    t.text     "concept",     limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title",       limit: 255
+    t.integer  "user_id",     limit: 4
+    t.integer  "likes_count", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
