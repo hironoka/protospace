0d75415d06d4416d61d5262523c5f155ead2bf1c
hirono <hotarak@gmail.com>
2016-10-15 04:50:51 +0900

    edit routing and controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,8 +4,6 @@ before_action :authenticate_user!, only: [:create, :destroy]
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @like = @prototype.likes.create(user_id: current_user.id)
-    # @like = Like.create(prototype_id: params[:prototype_id], user_id: current_user.id)
-    # @likes = Like.where(prototype_id: params[:prototype_id])
   end
 
   def destroy
