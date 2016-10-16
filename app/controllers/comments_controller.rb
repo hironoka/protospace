59165d70fa806cf6322127be1167355339ffaaca
hirono <hotarak@gmail.com>
2016-10-17 02:32:43 +0900

    delete @comment from comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.create(comment_params)
+    @prototype.comments.create(comment_params)
     @comments = @prototype.comments
   end
   private
