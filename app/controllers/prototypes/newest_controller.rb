b4a131cfdfbb823dd25e9ced36f7ba7315cd6b5a
hirono <hotarak@gmail.com>
2016-10-24 13:35:19 +0900

    set the pagination number in prototype.rb

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::NewestController < ApplicationController
 
   def index
-    @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page]).per(8)
+    @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page])
     render "prototypes/index"
   end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,7 +5,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.eager_load(:user, :main_image, :tags).page(params[:page]).per(8)
+    @prototypes = Prototype.eager_load(:user, :main_image, :tags).page(params[:page])
   end
 
   def new
